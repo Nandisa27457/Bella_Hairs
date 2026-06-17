b4c1571e1d3ad6f1705fa03abe2453299eee26c8
Nandisa Dlamini <shangenandisa@gmail.com>
2026-06-17 18:13:41 +0200

    NavBar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,29 @@
+
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <h2 className="font-display text-2xl tracking-tight">
+          Bella Hair
+        </h2>
+      </div>
+
+      <div className="nav-menu">
+        <ul className="nav-links">
+          <li><a href="/">Home</a></li>
+          <li><a href="/services">Services</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/gallery">Gallery</a></li>
+        </ul>
+
+        <button className="book-btn">
+          Book Now
+        </button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
