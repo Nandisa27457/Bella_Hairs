e53f77f4fa44c50550117bfde7eb1145897d6785
Nandisa Dlamini <shangenandisa@gmail.com>
2026-06-20 21:57:46 +0200

    Imported and added link on navbar links

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,7 @@
 
 import "./Navbar.css";
+import { Link } from "react-router-dom";
+
 
 function Navbar() {
   return (
@@ -11,11 +13,11 @@ function Navbar() {
       </div>
 
       <div className="nav-menu">
-        <ul className="nav-links">
-          <li><a href="/">Home</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/gallery">Gallery</a></li>
+        <ul className="nav-links">  
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/gallery">Gallery</Link></li>
         </ul>
 
         <button className="book-btn">
