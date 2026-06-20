3642a2b4d032e5a89289e6c69901048d1fd4db98
Nandisa Dlamini <shangenandisa@gmail.com>
2026-06-20 21:54:35 +0200

    Import and use of router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 
 import HomePage 
 from "./Pages/HomePage";
+import ServicesPage from "./Pages/Services";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import AboutPage from "./Pages/About";
 
 function App() {
   return (
-    <>
-    <HomePage />
-    
-    </>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </BrowserRouter>
+  );  
 }
 
 export default App;
